0c5b2d0a453089c9c7fbc0677df2133dca09ed91
ephreal <tjannx@hotmail.com>
2019-06-26 15:46:04 -0700

    sed is now all worky worky

--- scripts/setup_gnome_backgrounds.sh
+++ scripts/setup_gnome_backgrounds.sh
@@ -1,6 +1,6 @@
 mkdir -p $HOME/.local/share/systemd/user
 cp ../gnome_inspirobot_background/* $HOME/.local/share/systemd/user
-sed -i "s/HOME/$HOME/g" $HOME/.local/share/systemd/user/background.service
+sed -i "s%HOME%$HOME%g" $HOME/.local/share/systemd/user/background.service
 systemctl --user enable background.service
 
 # Set the first random background immediately
