d9636fc5731f7fa4212b46b2849e7c41d4796de5
ephreal <tjannx@hotmail.com>
2019-07-01 13:56:05 -0700

    Added atom environment setup for python

---
+++ scripts/setup_atom_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo pip install pylama pylama-pylint flake8
+
+apm install busy-signal
+apm install intentions
+apm install linter
+apm install linter-ui-default
+apm install linter-python
+apm install linter-flake8
+apm install script
