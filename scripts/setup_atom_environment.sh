7b9e0cc5f7bf81a718e9f7ac2a59c9477b91aa7c
ephreal <tjannx@hotmail.com>
2019-07-12 08:23:02 -0700

    setup_atom_environment.sh

--- scripts/setup_atom_environment.sh
+++ scripts/setup_atom_environment.sh
@@ -2,6 +2,9 @@
 
 sudo pip install pylama pylama-pylint flake8
 
+mkdir ~/.atom
+cp ../configfiles/atom.io/config.cson ~/.atom/
+
 apm install busy-signal
 apm install intentions
 apm install linter
@@ -9,3 +12,4 @@ apm install linter-ui-default
 apm install linter-python
 apm install linter-flake8
 apm install script
+apm install atom-python-virtualenv
