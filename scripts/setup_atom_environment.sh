edafadd3786fc06fd8f58e17821dff287ec07806
ephreal <tjannx@hotmail.com>
2019-11-22 10:49:34 -0700

    Included emmet install

--- scripts/setup_atom_environment.sh
+++ scripts/setup_atom_environment.sh
@@ -5,4 +5,4 @@ sudo pip install pylama pylama-pylint flake8
 mkdir ~/.atom
 cp ../configfiles/atom.io/config.cson ~/.atom/
 
-apm install busy-signal intentions linter linter-ui-default linter-python linter-flake8 script atom-python-virtualenv
+apm install atom busy-signal intentions linter linter-ui-default linter-python linter-flake8 script atom-python-virtualenv
