a9091ef52da817fbe953aebc4dc47f41438f9f1a
ephreal <tjannx@hotmail.com>
2020-11-25 21:56:36 -0700

    Added additional proton notes

--- scripts/ge_installer.sh
+++ scripts/ge_installer.sh
@@ -4,11 +4,13 @@ fi
 
 cd $HOME/.steam/steam/compatibilitytools.d
 
+# Note: For Magicka, I use Proton 4.2.9, no GE version.
+
 # Glorious Eggroll Versions that I use. I'll make it pull from the array in the
 # future. For now, I just want to hack this together so it works.
 versions=(
     "5.21-GE-1" # Warframe
-    "5.6-GE-2" # Assassin's Creed (all of them)
+    "5.6-GE-2" # Assassin's Creed (all of them), Satisfactory, Oblivion
     "5.4-GE-3" # Deep Rock Galactic
     "5.2-GE-2" # Dishonored, Dishonored 2
 )
