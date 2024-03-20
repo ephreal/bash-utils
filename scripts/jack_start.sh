4da41a7f4ed14b1d20ab95a12ba117a31ef8688a
ephreal <ephreal@mylifeneeds.management>
2024-03-20 08:33:20 -0700

    Pulling sources from github

--- scripts/jack_start.sh
+++ scripts/jack_start.sh
@@ -1,4 +1,5 @@
-sudo echo "Starting jack"
-sudo jackd -r -d dummy -r 48000 &
-sleep 5
+#sudo echo "Starting jack"
+#sudo jackd -r -d dummy -r 48000 &
+#sleep 5
+echo "Starting audio bridge for midi passthrough to lmms"
 a2j_control --ehw && a2j_control --start
